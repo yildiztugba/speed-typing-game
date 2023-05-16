8c646ff8575f7dbb59e3e4738090c2bf2e89ae88
yildiztugba <okra.yildiz@gmail.com>
2023-05-16 14:18:54 +0200

    html part of app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <div>
+            <h1>How fast do you type?</h1>
+            <textarea />
+            <h4>Time remaining: {}</h4>
+            <button>
+                Start
+            </button>
+            <h1>Word count: {}</h1>
+        </div>
   );
 }
 
